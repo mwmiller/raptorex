d90c55b3ac2642019bf9b6a68969b625073e5b84
Matt Miller <mwm@fastmail.com>
2019-12-04 19:15:28 +0200

    Add L computation
    I'm not sure I entirely like this layout going forward, but I don't
    know where everythign fits.  I'm trying to make it easy to follow the
    RFC notation inside the code, but some doesn't translate well.
    
    We also recompute some values and use less than optimal searches.. I
    might clean this up in an optimsation sweep, but I really want to get
    some test vectors before I change things to be less obvious/naive.

--- lib/raptorex.ex
+++ lib/raptorex.ex
@@ -2,4 +2,28 @@ defmodule Raptorex do
   @moduledoc """
   Documentation for Raptorex.
   """
+
+  alias Raptorex.Lookup.Primes
+
+  def x(k), do: intceil((:math.sqrt(8 * k + 1) + 1) / 2)
+  def s(k), do: Primes.smallestgte(intceil(0.01 * k) + x(k))
+  def h(k), do: find_h(2, k + s(k))
+  def l(k), do: k + h(k) + s(k)
+
+  # This search can be better, but I need some bounds
+  # to know where to search
+  defp find_h(n, sum) do
+    case choose(n, intceil(n / 2)) >= sum do
+      true -> n
+      false -> find_h(n + 1, sum)
+    end
+  end
+
+  defp intceil(x), do: x |> Float.ceil() |> trunc
+  defp choose(n, k), do: div(fac(n), fac(k) * fac(n - k))
+
+  defp fac(1), do: 1
+  defp fac(n), do: fac(n, n - 1)
+  defp fac(n, 1), do: n
+  defp fac(n, i), do: fac(n * i, i - 1)
 end
