6a3527f02625f764d759fff257547215b5b35e9f
Matt Miller <mwm@fastmail.com>
2019-12-05 07:06:57 +0200

    Add triple generator

--- lib/raptorex/generators.ex
+++ lib/raptorex/generators.ex
@@ -1,6 +1,9 @@
 defmodule Raptorex.Generators do
   use Bitwise
-  alias Raptorex.Lookup.{V0, V1}
+  alias Raptorex.Lookup.{J, Primes, V0, V1}
+
+  # the largest prime smaller than 2^^16.
+  @q 65521
 
   def rand(x, i, m),
     do: rem(V0.value(rem(x + i, 256)) ^^^ V1.value(rem(trunc(x / 256) + i, 256)), m)
@@ -13,4 +16,15 @@ defmodule Raptorex.Generators do
   def degree(n) when n < 1_032_189, do: 11
   def degree(n) when n < 1_048_576, do: 40
   def degree(_), do: :error
+
+  def triple(k, x) do
+    lp = Primes.smallestgte(Raptorex.l(k))
+    j = J.value(k)
+
+    a = rem(53591 + j * 997, @q)
+    b = rem(10267 * (j + 1), @q)
+    y = rem(b + x * a, @q)
+
+    {rand(y, 0, 1_048_576) |> degree(), 1 + rand(y, 1, lp - 1), rand(y, 2, lp)}
+  end
 end
